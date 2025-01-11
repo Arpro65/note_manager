afd32534c624d09e5f80729cb24baafcfd662c2a
Arpro65 <ark.sam65@gmail.com>
2025-01-11 23:10:36 +0300

    Add files via upload

--- display_notes_function.py
+++ display_notes_function.py
@@ -5,12 +5,22 @@ from colorama import Fore, Back, Style, init
 init(autoreset=True)
 
 
+def display_note(note, index):
+    print(Fore.YELLOW + f'Заметка №{index}:')
+    print(Fore.CYAN + f'Имя пользователя: {note["username"]}')
+    print(Fore.GREEN + f'Заголовок: {note["title"]}')
+    print(Fore.WHITE + f'Описание: {note["description"]}')
+    print(Fore.BLUE + f'Статус: {note["status"]}')
+    print(Fore.MAGENTA + f'Дата создания: {note["date_created"].strftime("%d-%m-%Y")}')
+    print(Fore.MAGENTA + f'Дедлайн: {note["deadline"].strftime("%d-%m-%Y")}')
+    print(Back.BLACK + '-' * 40 + Style.RESET_ALL)
+
+
 def display_notes(notes, page_size=5):
     if not notes:
         print(Fore.RED + "У вас нет сохранённых заметок." + Style.RESET_ALL)
         return
 
-    # Сортировка заметок по дате создания
     sorted_notes = sorted(notes, key=lambda note: note.get('date_created', ''))
 
     total_pages = math.ceil(len(sorted_notes) / page_size)
@@ -20,29 +30,28 @@ def display_notes(notes, page_size=5):
         start_index = (current_page - 1) * page_size
         end_index = min(start_index + page_size, len(sorted_notes))
 
-        for i in range(start_index, end_index):
-            note = sorted_notes[i]
-            print(Fore.YELLOW + f'Заметка №{i + 1}:')
-            print(Fore.CYAN + f'Имя пользователя: {note["username"]}')
-            print(Fore.GREEN + f'Заголовок: {note["title"]}')
-            print(Fore.WHITE + f'Описание: {note["description"]}')
-            print(Fore.BLUE + f'Статус: {note["status"]}')
-            print(Fore.MAGENTA + f'Дата создания: {note["date_created"].strftime("%d-%m-%Y")}')
-            print(Fore.MAGENTA + f'Дедлайн: {note["deadline"].strftime("%d-%m-%Y")}')
-            print(Back.BLACK + '-' * 40 + Style.RESET_ALL)
+        for index, note in enumerate(sorted_notes[start_index:end_index], start=start_index + 1):
+            display_note(note, index)
 
-        choice = input(
-            '\n' + Fore.YELLOW + f'Страница {current_page}/{total_pages}. Введите "н" для следующей страницы, "п" для предыдущей или "в" для выхода: ' + Style.RESET_ALL)
+        try:
+            choice = input(
+                '\n' + Fore.YELLOW + f'Страница {current_page}/{total_pages}. Введите "н" для следующей страницы, "п" для предыдущей или "в" для выхода: ' + Style.RESET_ALL).lower()
 
-        if choice == 'н':
-            current_page += 1
-        elif choice == 'п':
-            current_page -= 1
-        else:
+            if choice == 'н':
+                if current_page < total_pages:
+                    current_page += 1
+            elif choice == 'п':
+                if current_page > 1:
+                    current_page -= 1
+            elif choice == 'в':
+                break
+            else:
+                print(Fore.RED + "Неверная команда. Попробуйте снова." + Style.RESET_ALL)
+        except KeyboardInterrupt:
+            print("\nПрервано пользователем.")
             break
 
-        if current_page > total_pages or current_page <= 0:
-            break
+    print(Fore.GREEN + "Вы вышли из просмотра заметок." + Style.RESET_ALL)
 
 
 # Тестовый список заметок
